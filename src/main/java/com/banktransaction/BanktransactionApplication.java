4d61d78257b05dfaadf745932eff90587edf5c48
Ruben Dessimoni <rubendess@gmail.com>
2018-06-08 20:15:51 -0300

    Initial commit

---
+++ src/main/java/com/banktransaction/BanktransactionApplication.java
@@ -0,0 +1,12 @@
+package com.banktransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanktransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanktransactionApplication.class, args);
+	}
+}
